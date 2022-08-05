d271575dec313ed68dfe7164d2bfe9505645826d
ils15 <igorleite-ils@hotmail.com>
2022-08-05 19:17:26 -0300

    Upgrade Leveling Settings

--- SV03/2-Marlin Firmware/Marlin/Configuration.h
+++ SV03/2-Marlin Firmware/Marlin/Configuration.h
@@ -1509,7 +1509,7 @@
  * A total of 2 does fast/slow probes with a weighted average.
  * A total of 3 or more adds more slow probes, taking the average.
  */
-//#define MULTIPLE_PROBING 2
+#define MULTIPLE_PROBING 2
 //#define EXTRA_PROBING    1
 
 /**
@@ -1906,7 +1906,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 4
+  #define GRID_MAX_POINTS_X 5
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
